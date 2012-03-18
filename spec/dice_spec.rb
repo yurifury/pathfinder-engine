7eb51ed185f45bb832785c4a9b611a374cf0e35d
Yuri Feldman <yuri@tbqh.net>
2012-03-19 02:03:12 +1100

    First DRY of spec

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,23 +2,22 @@ require "dice"
 
 describe Dice do
   describe "d6" do
+    before do
+      @dice = Dice.new(6)
+      @rolls = []
+      50.times { @rolls << @dice.roll }
+    end
+
     it "should return between 1 and 6" do
-      dice = Dice.new(6)
-      dice.roll.should be_between(1, 6)
+      @dice.roll.should be_between(1, 6)
     end
 
     it "should return different numbers for new rolls" do
-      dice = Dice.new(6)
-      rolls = []
-      10.times { rolls << dice.roll }
-      rolls.uniq.size.should be > 2
+      @rolls.uniq.size.should be > 2
     end
 
     it "should not return 0 or 7" do
-      dice = Dice.new(6)
-      rolls = []
-      10.times { rolls << dice.roll }
-      rolls.should_not include(0, 7)
+      @rolls.should_not include(0, 7)
     end
   end
 end
