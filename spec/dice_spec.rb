445c080d242b1d7cf0d888934ca755dee7822ba7
Yuri Feldman <yuri@tbqh.net>
2012-03-19 02:28:14 +1100

    DRY up specs some more

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,22 +2,27 @@ require "dice"
 
 describe Dice do
   describe "d6" do
-    before do
-      @dice = Dice.new(6)
-      @rolls = []
-      50.times { @rolls << @dice.roll }
-    end
+    let (:dice) { Dice.new(6) }
 
     it "should return between 1 and 6" do
-      @dice.roll.should be_between(1, 6)
+      dice.roll.should be_between(1, 6)
     end
 
-    it "should return different numbers for new rolls" do
-      @rolls.uniq.size.should be > 2
-    end
+    describe "multiple rolls" do
+      let (:rolls) do
+        rolls = []
+        50.times { rolls << dice.roll }
+        rolls
+      end
 
-    it "should not return 0 or 7" do
-      @rolls.should_not include(0, 7)
+
+      it "should return different numbers for new rolls" do
+        rolls.uniq.size.should be > 2
+      end
+
+      it "should not return 0 or 7" do
+        rolls.should_not include(0, 7)
+      end
     end
   end
 end
