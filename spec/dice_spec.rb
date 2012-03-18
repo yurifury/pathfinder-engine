4f63a36534f9ca312819c5b82ed21721cc3314d7
Yuri Feldman <yuri@tbqh.net>
2012-03-19 03:15:05 +1100

    DRY up more code, write roll_many

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,25 +3,27 @@ require "dice"
 describe Dice do
   describe "d6" do
     let (:dice) { Dice.new(6) }
+    subject { dice }
 
-    it "should return between 1 and 6" do
-      dice.roll.should be_between(1, 6)
-    end
+    it { should respond_to(:roll) }
 
-    describe "multiple rolls" do
-      let (:rolls) do
-        rolls = []
-        50.times { rolls << dice.roll }
-        rolls
-      end
+    describe "rolls" do
+      subject { dice.roll }
 
+      it { should be_between(1, 6) }
+      it { should be_a(Integer) }
 
-      it "should return different numbers for new rolls" do
-        rolls.uniq.size.should be > 2
-      end
+      describe "multiple rolls" do
+        let (:rolls) { dice.roll_many(30) }
+        subject { rolls }
 
-      it "should not return 0 or 7" do
-        rolls.should_not include(0, 7)
+        it { should be_a(Array) }
+        its(:size) { should be 30 }
+        it { should_not include(0, 7, 25) }
+
+        it "should not just be one repeated value" do
+          rolls.uniq.size.should be > 4
+        end
       end
     end
   end
